cadbbc6a2ff59137465c386885ef683b6e205614
georgetxy265 <georgetxy265@gmail.com>
2026-09-24 22:06:48 +0800

    Add automatic deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   allowedDevOrigins: ["192.168.18.82"],
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
